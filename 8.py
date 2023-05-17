277a4de6b119f46761b15d4f2d21d803d7409d9c
alicia8989 <114919948+alicia8989@users.noreply.github.com>
2023-05-17 13:39:29 +0200

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,16 @@
+from microbit import *
+from maqueen import*
+
+robot= Maqueen()
+
+robot.girar_derecha()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
